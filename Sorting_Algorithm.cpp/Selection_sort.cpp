abc38c01de9e2325c888d5d16c2675548b58f5f2
bornabhowmik <bornabhowmik4@gmail.com>
2023-10-03 19:11:03 +0600

    Selection_sort.cpp code added

--- Sorting_Algorithm.cpp/Selection_sort.cpp
+++ Sorting_Algorithm.cpp/Selection_sort.cpp
@@ -6,16 +6,14 @@ int main(){
     cin>>n;
 
     int arr[n];
-    
     for(int i=0; i<n; i++){
-
         cin>>arr[i];
     }
 
     for(int i=0; i<n-1; i++){
 
         for(int j=i+1; j<n; j++){
-
+        
             if(arr[j] < arr[i]){
                 
                 int temp=arr[j];
