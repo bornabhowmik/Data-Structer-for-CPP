65596bf1a655386f77d54d11444607a6a1e94eb3
bornabhowmik <bornabhowmik4@gmail.com>
2023-10-07 16:18:58 +0600

    lcs4.cpp code added

---
+++ Dynamic_Programming.cpp/lcs4.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void lcs(char *s1, char *s2, int m, int n){
+    int table[m+1][n+1];
+
+    for(int i=0; i<=m; i++){
+        for(int j=0; j<=n; j++){
+
+            if(i==0 || j==0){
+                table[i][j]=0;
+
+            }else if(table[i-1]==table[j-1]){
+                table[i][j]=table[i-1][j-1]+1;
+
+            }else{
+                table[i][j]=max(table[i-1][j], table[i][j-1]);
+            }
+
+            cout<<table[i][j]<<" ";
+        }
+
+        cout<<endl;
+    }
+}
+
+int main(){
+
+    char s1[]="abcdef";
+    char s2[]="bcde";
+
+    int m=strlen(s1);
+    int n=strlen(s2);
+
+    lcs(s1, s2, m, n);
+}
