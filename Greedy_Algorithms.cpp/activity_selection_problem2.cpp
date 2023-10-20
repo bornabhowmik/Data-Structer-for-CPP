19183c8e14fe8ec777b6fc0f0d3c01b584e5add0
bornabhowmik <bornabhowmik4@gmail.com>
2023-10-20 20:18:29 +0600

    activity_selection4.cpp code added

---
+++ Greedy_Algorithms.cpp/activity_selection_problem2.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Activity{
+    int start;
+    int finish;
+};
+
+bool compareActivities(const Activity& a , const Activity& b ){
+    return a.finish<b.finish;
+}
+
+void selectActivities(vector <Activity>& activities){
+    sort(activities.begin(), activities.end(), compareActivities);
+
+    int selected=0;
+
+    cout<<"activity:" << "(";
+    cout<<activities[selected].start<<","<<activities[selected].finish<<")";
+
+    for(int i=0; i<activities.size(); i++){
+        if(activities[selected].finish<=activities[i].start){
+            selected=i;
+
+            cout<<"("<<activities[selected].start<<","<<activities[selected].finish<<")";
+        }
+    }
+
+    cout<<endl;
+}
+
+int main(){
+
+    vector<Activity>activity={
+        {1,3},
+        {2,4},
+        {3,5},
+        {4,6},
+        {4,7},
+    };
+    selectActivities(activity);
+}
